5981da89140627b8678081d2b4374d578d951d3a
chmery <karektv@gmail.com>
2023-03-20 20:24:23 +0100

    responsiveness for navigation component

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,4 +1,5 @@
 import { Link, useLocation } from "react-router-dom";
+import { CartIcon, PlusIcon } from "../Icons/Icons";
 import styles from "./Navigation.module.css";
 
 const Navigation = () => {
@@ -8,11 +9,17 @@ const Navigation = () => {
     return (
         <nav className={styles.navigation}>
             <div className={`${styles.item} ${parentPath === "carts" ? styles.active : ""}`}>
-                <Link to={"carts"}>All Carts</Link>
+                <Link to={"carts"} className={styles.link}>
+                    <CartIcon />
+                    <span className={styles.text}>All Carts</span>
+                </Link>
                 {parentPath === "carts" && <div className={styles["active-bar"]} />}
             </div>
             <div className={`${styles.item} ${parentPath === "creator" ? styles.active : ""}`}>
-                <Link to={"creator"}>Create New Cart</Link>
+                <Link to={"creator"} className={styles.link}>
+                    <PlusIcon />
+                    <span className={styles.text}>Create New Car</span>
+                </Link>
                 {parentPath === "creator" && <div className={styles["active-bar"]} />}
             </div>
         </nav>
